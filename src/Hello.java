983bf508940b8347fe0f1ae9169b704a7c996acc
zammaliahmed <zammaliahmed.enit@gmail.com>
2022-11-18 11:41:17 +0100

    2er modif

--- src/Hello.java
+++ src/Hello.java
@@ -1,14 +1,19 @@
 
 public class Hello {
 	int age;
+	int nom ;
 
 	public int getAge() {
 		return age;
 	}
-
+    public int getnom() {
+		return nom;
+	}
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+	public void setnom(int nom) {
+		this.nom = nom;
+	}
 	
 }
--- src/Home.java
+++ src/Home.java
@@ -4,6 +4,7 @@ public class Home {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello2");
+		System.out.println("Hello3");
 
 	}
 
